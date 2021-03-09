2772c40cc8f9ba12d905e76a0e7e240a27568f2a
hazrat <khnnibir@gmail.com>
2021-03-09 13:04:07 +0600

    first commit

---
+++ scripts.js
@@ -0,0 +1,62 @@
+function kilomiterMiter(kilo){
+var miter=kilo*1000;
+return miter;
+
+}
+var result=kilomiterMiter(2);
+
+
+
+
+
+        
+function resturantCost(day){
+   var cost=0;
+   if(day<=10){
+    var cost=day*100;
+
+   }else if(day<=20){
+       var firstDay=10*100;
+       var remaining=day-10;
+       var secondDay=remaining*80;
+       var cost=firstDay+secondDay;
+
+   }else{
+    var firstDay=10*100;
+    var secondDay=10*80;
+    var remaining=day-20;
+    var thirdDay=remaining*50;
+    var cost=firstDay+secondDay+thirdDay;
+    
+   
+
+   }
+   return cost;
+
+}
+
+var result=resturantCost(26);
+
+
+
+function nameList(arry){
+  var word='hazratali';
+  for( var x=0; x<arry.length; x++){
+    if(word.length<arry[x].length){
+      word=arry[x];
+    }
+
+  }
+
+  return word;
+
+
+}
+
+var result=nameList(['hazrat','nibir','ali'])
+
+
+
+
+
+
